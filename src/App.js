e0832910d589ae7183aee34f900c2fd383618453
macfrei <lene@7freiheiten.de>
2021-02-20 12:00:18 +0100

    Add globalstyles, appheader and appgrid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import styled from 'styled-components'
+import Header from './components/AppHeader'
+
 function App() {
-  return <div className="App">Hello World</div>
+  return (
+    <AppGrid>
+      <Header>Shopping Cart 👛</Header>
+    </AppGrid>
+  )
 }
 
 export default App
+
+const AppGrid = styled.div`
+  display: grid;
+  grid-template-rows: 40px auto;
+  grid-gap: 20px;
+  height: 100vh;
+`
---
+++ src/components/AppHeader.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/macro'
+
+const Header = styled.header`
+  display: flex;
+  justify-content: flex-end;
+  border-bottom: 1px solid #badaba;
+  padding: 8px;
+`
+export default Header
